2a2e3aabf3408c14a4e31cb6da982b6173e0b32a
YasserIbrahem1997 <sokara57@gmail.com>
2024-12-04 12:51:22 +0200

    push new verison

--- lib/features/home/widgets/web/web_visit_again_view_widget.dart
+++ lib/features/home/widgets/web/web_visit_again_view_widget.dart
@@ -19,7 +19,7 @@ class WebVisitAgainView extends StatefulWidget {
 }
 
 class _WebVisitAgainViewState extends State<WebVisitAgainView> {
-  final CarouselController carouselController = CarouselController();
+  final CarouselSliderController  carouselController = CarouselSliderController();
 
   @override
   Widget build(BuildContext context) {
